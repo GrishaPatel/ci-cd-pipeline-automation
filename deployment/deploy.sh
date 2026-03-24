03646a1f028e676d6a635c99b03917c395951fa6
GrishaPatel <patelgrisha30@gmail.com>
2026-03-24 19:17:06 +0530

    Update CI/CD configuration

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -5,12 +5,12 @@ echo "Starting deployment..."
 docker stop ci_cd_flask_app || true
 docker rm ci_cd_flask_app || true
 
-docker pull your-dockerhub-PatelGrisha/ci-cd-pipeline:latest
+docker pull PatelGrisha/ci-cd-pipeline:latest
 
 docker run -d \
   --name ci_cd_flask_app \
   -p 5000:5000 \
-  your-dockerhub-PatelGrisha/ci-cd-pipeline:latest
+  PatelGrisha/ci-cd-pipeline:latest
 
 echo "Deployment completed successfully!"
 
